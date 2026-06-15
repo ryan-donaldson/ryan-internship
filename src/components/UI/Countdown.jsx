d6ebc2efc275663cfab17a86f571d0a56b12c157
ryan-donaldson <ryanwblue@gmail.com>
2026-06-15 17:21:07 -0600

    Implemented New Items section with carousel and loading state

---
+++ src/components/UI/Countdown.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import updateTime from "../home/NewItems"
+
+function Countdown({ expiryDate }) {
+  const [time, setTime] = useState(updateTime(expiryDate));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(updateTime(expiryDate));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [expiryDate]);
+  
+  return <div className="de_countdown">{time}</div>;
+}
+
+export default Countdown;
